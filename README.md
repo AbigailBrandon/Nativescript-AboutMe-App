1a9739c65bf3dd9779d3832409faba0312fe5faf
AbigailBrandon <143516219+AbigailBrandon@users.noreply.github.com>
2023-10-25 12:12:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nativescript-AboutMe-App
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/AbigailBrandon/Nativescript-AboutMe-App)
---
+++ app/main-page.js
@@ -0,0 +1,24 @@
+import { createViewModel } from './main-view-model';
+
+
+import { Frame } from "@nativescript/core";
+
+export function onAboutTap() {
+  const topmost = Frame.topmost();
+  topmost.navigate("about-page");
+}
+
+export function onSkillsTap() {
+  const topmost = Frame.topmost();
+  topmost.navigate("skills-page");
+}
+
+export function onContactTap() {
+  const topmost = Frame.topmost();
+  topmost.navigate("contact-page");
+}
+
+export function onPortfolioTap() {
+  const topmost = Frame.topmost();
+  topmost.navigate("portfolio-page");
+}
