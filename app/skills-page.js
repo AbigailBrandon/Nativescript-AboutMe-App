df8e436993e0fb2ab18b9e863734809099f1e785
AbigailBrandon <abigailbrandonn@gmail.com>
2023-11-04 13:13:27 -0500

    added skills page content, added listviews

--- app/skills-page.js
+++ app/skills-page.js
@@ -1,33 +1,48 @@
 function onPageLoaded(args) {
     const page = args.object;
-    
+
     const timelineItems = [
         {
             year: '2023 - Present',
             entries: [
                 { title: 'Front-End Developer', subtitle: 'LimeLight Marketing' },
                 { title: 'Project Manager', subtitle: 'Pitt State CS Club' },
-            ]
+            ],
         },
         {
             year: '2022 - 2023',
             entries: [
                 { title: 'Webflow Developer & Co-Founder', subtitle: 'Designflow Studio' },
                 { title: 'Web Designer', subtitle: 'Freelance' },
-                { title: 'Leader Team of Marketing', subtitle: 'AHA Marketing Agency' }
-            ]
+                { title: 'Leader Team of Marketing', subtitle: 'AHA Marketing Agency' },
+            ],
         },
         {
             year: '2010 - 2013',
             entries: [
-                { title: 'Bachelor Degree of Information Technology', subtitle: 'US RMIT University' }
-            ]
-        }
+                { title: 'Bachelor Degree of Information Technology', subtitle: 'US RMIT University' },
+            ],
+        },
+    ];
+
+    const skills = [
+        { name: 'HTML', icon: '➤' },
+        { name: 'CSS', icon: '➤' },
+        { name: 'Java', icon: '➤' },
+        { name: 'Tailwind', icon: '➤' },
+        { name: 'Bootstrap', icon: '➤' },
+        { name: 'Squarespace', icon: '➤' },
+        { name: 'Wix', icon: '➤' },
+        { name: 'Adobe Lightroom', icon: '➤' },
+        { name: 'Adobe Photoshop', icon: '➤' },
     ];
     
+
     page.bindingContext = {
-        timelineItems: timelineItems
+        timelineItems: timelineItems,
+        skills: skills,
     };
 }
 
 exports.onPageLoaded = onPageLoaded;
+
